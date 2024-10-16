eba735b3d17f36fd881dc4c219b80d4de77ef035
andersonbalves <baratella.anderson@gmail.com>
2024-10-16 16:29:28 -0300

    Commit inicial

---
+++ app/src/main/java/com/baratella/piquis/PiquisApplication.java
@@ -0,0 +1,13 @@
+package com.baratella.piquis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiquisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PiquisApplication.class, args);
+  }
+
+}
---
+++ app/src/test/java/com/baratella/piquis/PiquisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baratella.piquis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiquisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
